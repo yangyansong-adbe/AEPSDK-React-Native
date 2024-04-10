f34e756fc14c1a7809f870931dc4dfd405eeef6b
yansong <yanyang@adobe.com>
2024-04-10 10:49:48 -0500

    no message

--- apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.h
+++ apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.h
@@ -1,6 +1,23 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
+@import AEPCore;
+@import AEPServices;
+@import AEPSignal;
+@import AEPLifecycle;
+@import AEPIdentity;
+@import AEPUserProfile;
+@import AEPMessaging;
+@import AEPEdge;
+@import AEPEdgeIdentity;
+@import AEPEdgeConsent;
+@import AEPEdgeBridge;
+@import AEPAssurance;
+@import AEPOptimize;
+@import AEPPlaces;
+@import AEPTarget;
+@import AEPCampaignClassic;
+
 @interface AppDelegate : RCTAppDelegate
 
 @end
